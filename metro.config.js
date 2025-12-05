01f20a19d4cff41bcfa0f97aee90eb84163003a1
HarshK97 <harshkapse1234@gmail.com>
2025-12-05 11:59:18 +0530

    init to win it.

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,10 @@
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require("nativewind/metro");
+const path = require("path");
 
 const config = getDefaultConfig(__dirname);
+
+config.resolver.nodeModulesPaths = [path.resolve(__dirname, "node_modules")];
+config.watchFolders = [path.resolve(__dirname, "node_modules")];
 
 module.exports = withNativeWind(config, { input: "./app/globals.css" });
